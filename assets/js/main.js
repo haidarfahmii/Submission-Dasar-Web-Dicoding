0463c6e8c6e54049a73b22b2b110f4350fac026a
Haidar Fahmi <haidarfahmi2000@gmail.com>
2021-02-18 11:05:42 +0700

    Commit Final v1.0

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+function changeBasic1(){
+    document.getElementById("iconBasic").src="assets/img/node.png";
+}
+function changeBasic2(){
+    document.getElementById("iconBasic").src="assets/img/react.png";
+}
+function changeIntermediate1(){
+    document.getElementById("iconIntermediate").src="assets/img/css.png";
+}
+function changeIntermediate2(){
+    document.getElementById("iconIntermediate").src="assets/img/html.png";
+}
+function changeAdvance1(){
+    document.getElementById("iconAdvance").src="assets/img/none.png";
+}
+function changeAdvance2(){
+    document.getElementById("iconAdvance").src="assets/img/none.png";
+}
